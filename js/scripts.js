7deb926009a9bac2666343808e2b9108445225d9
Zach Weintraub <zachweintraub@gmail.com>
2019-03-29 12:52:15 -0700

    add results to html and code result reveal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,11 @@ function checkComplete(a, b) {
 
 function calculateResult() {
   totalScore = q1 + q2 + q3 + q4 + q5 + q6;
-  if(totalScore > 0 && totalScore < 7) {
+  if(totalScore > 5 && totalScore < 11) {
     result = 'result1';
-  } else if(totalScore > 6 && totalScore < 13) {
+  } else if(totalScore > 10 && totalScore < 15) {
     result = 'result2';
-  } else if (totalScore > 12) {
+  } else if (totalScore > 14) {
     result = 'result3';
   }
   return result;
@@ -28,10 +28,12 @@ function calculateResult() {
 
 //and now for the ui logic
 $(function(){
+
   $('#start-btn').click(function(){
     $('#intro-text').slideUp();
     $('#category1').slideDown();
   });
+
   $('#btn1').click(function(){
     q1 = parseInt($("input:radio[name=q1a]:checked").val());
     q2 = parseInt($("input:radio[name=q1b]:checked").val());
@@ -42,24 +44,26 @@ $(function(){
       $('span#category1error').show();
     }
   });
+
   $('#btn2').click(function(){
     q3 = parseInt($("input:radio[name=q2a]:checked").val());
     q4 = parseInt($("input:radio[name=q2b]:checked").val());
-    $('#category2').slideUp();
-    $('#category3').slideDown();
+    if(checkComplete(q3, q4)) {
+      $('#category2').slideUp();
+      $('#category3').slideDown();
+    } else {
+      $('span#category2error').show();
+    }
   });
+
   $('#btn3').click(function(){
     q5 = parseInt($("input:radio[name=q3a]:checked").val());
     q6 = parseInt($("input:radio[name=q3b]:checked").val());
-    $('#category3').slideUp();
-    $('#' + calculateResult).slideDown();
+    if(checkComplete(q5, q6)) {
+      $('#category3').slideUp();
+      $('#' + calculateResult()).slideDown();
+    } else {
+      $('span#category3error').show();
+    }
   });
 });
-
-// the following is for reference
-// q1 = parseInt($("input:radio[name=q1a]:checked").val());
-// q2 = parseInt($("input:radio[name=q1b]:checked").val());
-// q3 = parseInt($("input:radio[name=q2a]:checked").val());
-// q4 = parseInt($("input:radio[name=q2b]:checked").val());
-// q5 = parseInt($("input:radio[name=q3a]:checked").val());
-// q6 = parseInt($("input:radio[name=q3b]:checked").val());
